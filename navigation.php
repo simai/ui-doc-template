ff8bf06c79ffc7fcd5fdbb32a3694e729289ff5b
Morlok8~ <maik3786@gmail.com>
2025-02-21 17:03:59 +0300

    changes to test and navigation

--- navigation.php
+++ navigation.php
@@ -8,7 +8,9 @@ return [
             'Navigation' => 'docs/navigation',
             'Algolia DocSearch' => 'docs/algolia-docsearch',
             'Custom 404 Page' => 'docs/custom-404-page',
+            
         ],
     ],
+    'Test' => 'docs/test',
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
 ];
