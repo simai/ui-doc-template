5d65ab1d2337781cce51c745e82fd522a8881933
Morlok8~ <maik3786@gmail.com>
2025-05-28 14:25:07 +0300

    added new pages

---
+++ source/_docs-ru/concept/.settings.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+            'title' => 'Концепции',
+            'menu' => [
+                'modifiers' => 'Модификаторы',
+                'control_points' => 'Контрольные точки',
+                'modifier_properties' => 'Свойства модификаторов',
+                'modifier_parameters' => 'Параметры модификаторов',
+                'modifier_limits' => 'Ограничения модификаторов',
+                'modifier_values' => 'Значения модификаторов',
+                'modifier_variables' => 'Переменные',
+                'color' => 'Цвет',
+                'color_primitives' => 'Цветовые примитивы',
+            ]
+    ];
+
