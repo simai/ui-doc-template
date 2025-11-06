98bfb73ce6d28cc8331a1395d16472c3676df4d2
Burundug <iburundugi@gmail.com>
2025-11-06 12:49:24 +0300

    update docs

--- source/docs/en/.settings.php
+++ source/docs/en/.settings.php
@@ -2,5 +2,23 @@
     return [
         'title' => 'Installation',
         'showInMenu' => false,
-        'order' => 1
+        'order' => 1,
+        'menu' =>
+            array (
+                'local-development' => 'Local Development',
+                'generating-your-site' => 'Generating Your Site',
+                'compiling-assets' => 'Compiling Assets',
+                'content' => 'Creating Your Site’s Content',
+                'content-translation-engine' => 'Content Translation Engine',
+                'configurator' => 'Configurator',
+                'registering-custom-tags' => 'Registering Custom Tags',
+                'site-variables' => 'Site Variables',
+                'helper-methods' => 'Helper Methods',
+                'page-metadata' => 'Page Metadata',
+                'collections' => 'Collections',
+                'pretty-urls' => 'Pretty URLs',
+                'custom-404-page' => 'Custom 404 Page',
+                'event-listeners' => 'Event Listeners',
+                'deploying-your-site' => 'Deploying Your Site',
+            ),
     ];
